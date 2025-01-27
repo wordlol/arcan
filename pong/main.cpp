65b1d014db8c7ca42a04078c7184e80902c951e4
0f0x64@gmail.com <0f0x64@gmail.com>
2025-01-27 22:00:14 +0700

    minimal

--- pong/main.cpp
+++ pong/main.cpp
@@ -1,11 +1,10 @@
 #include "windows.h"
+#include "math.h"
 
 HWND hWnd;
 
-int x = 555;
-int y = 555;
-int w = 100;
-int h = 100;
+float x = 0;
+float y = 0;
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
@@ -13,18 +12,29 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_ int       nCmdShow)
 {
     
-    hWnd = CreateWindow("game", 0, WS_POPUP| WS_VISIBLE|WS_MAXIMIZE, 0, 0, 0, 0, 0, 0, 0, 0);
+    hWnd = CreateWindow("edit", 0, WS_POPUP | WS_VISIBLE, x, y, 30, 30, 0, 0, 0, 0);
     UpdateWindow(hWnd);
-    ShowWindow(hWnd, SW_SHOW);
 
     while (!GetAsyncKeyState(VK_ESCAPE))
     {
-        //SetWindowPos(hWnd, NULL, x, y, w, h, 0);
-        ShowWindow(hWnd, SW_SHOW);
+        POINT p;
+        GetCursorPos(&p);
+        float dx = p.x - x;
+        float dy = p.y - y;
+        float len = sqrt(dx * dx + dy * dy);
+        
+        if (len < 15) {
+            MessageBox(hWnd, "game over", "!", 0);
+            x = 0;
+            y = 0;
+        }
+
+        len *= .1;
+        dx /= len;
+        dy /= len;
+        x += dx;
+        y += dy;
+        SetWindowPos(hWnd, NULL, x, y, 0, 0, SWP_NOSIZE);
         Sleep(16);//ждем 16 милисекунд (1/количество кадров в секунду)
-       
-
-
     }
-
 }
